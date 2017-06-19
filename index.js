1b914741eb1c57a1ffa33ed9a79c479cc7aff824
Rafael Escala <rafaesc92@gmail.com>
2017-06-19 18:19:38 -0500

    Deleted babel cacheDirectory

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ module.exports = [
 	{
 		test: /\.js$/,
 		exclude: /node_modules/,
-		loader: 'babel-loader?cacheDirectory=true',
+		loader: 'babel-loader',
 		options: {
 			"presets": [
 				["env", { "modules": false }],
